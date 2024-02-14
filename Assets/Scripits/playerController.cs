df66cc89eb51dede1cd8568b2888c207dbd087f7
1900046 <1900046@TE-GDA-D-D36G09>
2024-02-14 15:03:02 +0100

    Dodaj pliki projektów.

---
+++ Assets/Scripits/playerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += new Vector3(1, 0, 0) * Time.deltaTime ;
+        //mozna prosciej: Vector3.right
+
+
+        //zczytaj klawiature w osi poziomej:
+        float x = Input.GetAxis("Horizontal");
+        //wyswietl w konsoli stan klawiatury
+        Debug.Log(x);
+
+        //wylicz przesuniecie w osi x
+        Vector3 movement = Vector3.right * x;
+
+        //zczytaj z klawiatury os y
+        float y = Input.GetAxis("Vertical");
+
+        //wylicz przesuniecie w osi y i dodaj do istniejacego przesuniecia w osi x
+        
+        movement += Vector3.forward * y;
+
+        //normalizujemy wektor
+        movement = movement.normalized;
+
+        //poprawka ba czas ostatniej klatki
+        movement *= Time.deltaTime;
+
+        //pomnoz przez predkosc ruchu
+        movement *= moveSpeed;
+
+        //przesun gracza w osi x
+        //transform.position += movement;
+        
+
+        //probujemy uzyc translateaa
+
+
+
+       
+    }
+}
