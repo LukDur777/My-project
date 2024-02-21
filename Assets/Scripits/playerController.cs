53a8b76bdc561694935f9bc90db98ac4e77d5f82
Łukasz Durawa <lukasz.durawa@teb.edu.pl>
2024-02-21 14:26:14 +0100

    +Jump

--- Assets/Scripits/playerController.cs
+++ Assets/Scripits/playerController.cs
@@ -1,55 +1,69 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    //silnik fizyczny dla obiektu gracza
+    Rigidbody rb;
+    //si�a skoku
+    public float jumpForce = 5f;
+
     public float moveSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //przypnij rigidbody gracza do zmiennej rb
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //transform.position += new Vector3(1, 0, 0) * Time.deltaTime ;
-        //mozna prosciej: Vector3.right
+        //transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
+        //mo�na pro�ciej: Vector3.right
 
+        //zczytaj klawiatur� w osi poziomej:
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        //wy�wietl w konsoli stan klawiatury
+        Debug.Log(horizontalInput);
 
-        //zczytaj klawiature w osi poziomej:
-        float x = Input.GetAxis("Horizontal");
-        //wyswietl w konsoli stan klawiatury
-        Debug.Log(x);
+        //wylicz przesuni�cie w osi x
+        Vector3 movement = Vector3.right * horizontalInput;
 
-        //wylicz przesuniecie w osi x
-        Vector3 movement = Vector3.right * x;
+        //zczytaj z klawiatury o� y
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
-        //zczytaj z klawiatury os y
-        float y = Input.GetAxis("Vertical");
-
-        //wylicz przesuniecie w osi y i dodaj do istniejacego przesuniecia w osi x
-        
-        movement += Vector3.forward * y;
+        //wylicz przesuni�cie w osi y i dodaj do istniej�cego przesuni�cia w osi x
+        movement += Vector3.forward * verticalInput;
 
         //normalizujemy wektor
         movement = movement.normalized;
-
-        //poprawka ba czas ostatniej klatki
+        //poprawka na czas od ostatniej klatki
         movement *= Time.deltaTime;
-
-        //pomnoz przez predkosc ruchu
+        //pomn� przez pr�dko�� ruchu
         movement *= moveSpeed;
 
-        //przesun gracza w osi x
-        transform.position += movement;
-        
+        //przesu� gracza w osi x
+        //transform.position += movement;
 
-        //probujemy uzyc translateaa
+        //pr�bujemy u�yc translate zamiast dodawac wsp�rz�dne
+        transform.Translate(movement);
 
 
-
-       
+    }
+    //spr�bujmy obej�� problem z op�nieniem wej�cia poprzez przeniesienie go do update
+    void Update()
+    {
+        //sprawdz czy nacisnieto spacj� (skok)
+        //zwraca true je�li zacz�li�my naciska� spacj� w trakcie klatki animacji
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
